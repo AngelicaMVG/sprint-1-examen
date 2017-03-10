5557b9564f0e7c58004f82470d10e821eff476a1
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-10 15:11:21 -0600

    proyecto sprint1 final

--- js/app.js
+++ js/app.js
@@ -1,45 +1,22 @@
 var estudiantes = [];
-function obtenerListaEstudiantes( ) {
+function obtenerListaEstudiantes( ){
     // TO DO: Retornar la lista de estudiantes
-
     return estudiantes;
 }
 
 function agregarEstudiante() {
     // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
-    var preguntas = ['Nombre: ', 'Puntos Tecnicos: ', 'Puntos HSE: '];
-    var estudiante = [];
-   for( var i = 0; i < preguntas.length; i++){
-      var estudianteN = prompt(preguntas[i]).toLowerCase().trim();
+    var nombreDelEstudiante = (prompt('Nombre del Estudiante:')).toUpperCase();
+    var puntajeTecnico = prompt( 'Puntos Tecnicos del Estudiante:');
+    var puntajeHse = prompt('Puntos HSE del Estudiante');
 
-    };
-    var estudiante = new Object()
-         estudianteN.nombre = preguntas[0],
-         estudianteN.punTec =preguntas[1],
-         estudianteN.punHse = preguntas[2]
-
-
-
-    /*var estudiante = [];
-      var nombre = prompt(preguntas[0]);
-      var punTec = prompt(preguntas[1]);
-      var punHse = prompt(preguntas[2]);*/
-  /*  var estudianteN = new Object()
-         estudianteN.nombre = nombre,
-         estudianteN.punTec = punTec,
-         estudianteN.punHse = punHse*/
-
-
-
-
-      /*estudianteN = {
-
-    var  nombre = prompt(preguntas[0]),
-      var punTec = prompt(preguntas[1]),
-      var punHse = prompt(preguntas[2]);
-
-    }*/
-
+    var estudiante = {
+      nombreDelEstudiante: nombreDelEstudiante,
+      puntajeTecnico: puntajeTecnico,
+      puntajeHse: puntajeHse
+    }
+    estudiantes.push(estudiante);
+    return estudiante;
 
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
@@ -52,9 +29,9 @@ function mostrar(estudiante) {
     resultado += "<div class='col m12'>";
     resultado += "<div class='card blue-grey darken-1'>";
     resultado += "<div class='card-content white-text'>";
-    resultado += "<p><strong>Nombre:</strong> " + estudiante.nombre + "</p>";
-    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante + "</p>";
-    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante + "</p>";
+    resultado += "<p><strong>Nombre:</strong> " + estudiante.nombreDelEstudiante + "</p>";
+    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.puntajeTecnico + "</p>";
+    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.puntajeHse + "</p>";
     resultado += "</div>";
     resultado += "</div>";
     resultado += "</div>";
@@ -65,23 +42,37 @@ function mostrar(estudiante) {
 function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
+    var listaEstudiantes = '';
+    estudiantes.forEach(function(alumno){
+      listaEstudiantes += mostrar(alumno)
+    });
+    return listaEstudiantes;
 
 }
 
 function buscar(nombre, estudiantes) {
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
-
+    var estudianteFiltrado = estudiantes.filter(function(alumno){
+      return alumno.nombreDelEstudiante.toLowerCase() == nombre.toLowerCase()
+    });
+    return estudianteFiltrado;
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
 
 }
 
 function topTecnico(estudiantes) {
-    // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-
+    // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor   \
+    var puntajeTopTecnico = estudiantes.sort(function(item1, item2){
+      item2.puntajeTecnico - item1.puntajeTecnico
+    });
+    return puntajeTopTecnico;
 }
 
 function topHSE(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-
+    var puntajeTopHse = estudiantes.sort( function(item1, item2){
+      item2.puntajeHse - item1.puntajeHse
+    });
+    return puntajeTopHse;
 }
